0bb4dc841e8b38c5a7ee95c6f9a72552a7214bdb
William C. Canin <william.costa.canin@gmail.com>
2016-01-16 03:26:58 -0200

    Deploy: 2016-01-16T03:26:58

---
+++ src/javascripts/contact.js
@@ -0,0 +1,6 @@
+// Requerid fields
+jQuery(document).ready(function($) {
+    $('.name-fs input').prop('required',true);
+    $('.email-fs input').prop('required',true);
+    $('.content-fs textarea').prop('required',true);
+});
--- src/javascripts/formspree.js
+++
@@ -1,6 +0,0 @@
-// Requerid fields
-jQuery(document).ready(function($) {
-  $('form#formspree-contact').find('input,textarea').each(function(){
-    $(this).prop('required',true);
-  });
-});
